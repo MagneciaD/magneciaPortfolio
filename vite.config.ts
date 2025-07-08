9e81cf033e91d7578cf2a636c91c2d387978c7ee
MagneciaD <Magneciadladla@gmail.com>
2025-07-08 14:10:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/dladla-verse-architect-main/', 
   server: {
     host: "::",
     port: 8080,
