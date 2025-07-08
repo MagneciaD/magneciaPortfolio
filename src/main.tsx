a7dca9e7f12a1b6f702f406dde53ba12b58a13ab
MagneciaD <Magneciadladla@gmail.com>
2025-07-08 16:49:20 +0200

    another change

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { HashRouter } from 'react-router-dom'; // ✅ Add this import
+
+createRoot(document.getElementById("root")!).render(
+  <HashRouter> {/* ✅ Wrap App in HashRouter */}
+    <App />
+  </HashRouter>
+);
